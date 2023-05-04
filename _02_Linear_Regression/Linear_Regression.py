08a250812474af5105d84e2569e2bff0d25ee2e4
chengfan2006040233 <131932921+chengfan2006040233@users.noreply.github.com>
2023-05-04 15:19:44 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -12,31 +12,33 @@ def ridge(data):
     X,y = read_data()
     w =  np.matmul(np.linalg.inv(np.matmul(X.T,X)),np.matmul(X.T,y))
     return w@data
-    
+
 def lasso(data):
-     x,y = read_data()
+    x, y = read_data()
+    miu = np.mean(x)
+    sigma = np.std(x)
+    for i in range(x.shape[0]):
+        x[i] = (x[i] - miu) / sigma
+
+    y = np.array([y]).transpose()
+  
+    lamda = 1
+    a = 0.01
     epochs = 1000
-    a = 0.00001
-    Lambda = 1
-    #计算总数据量
-        m=x.shape[0]
-        #给x添加偏置项
-        X = np.concatenate((np.ones((m,1)),x),axis=1)
-        #计算总特征数
-        n = X.shape[1]
-        #初始化W的值,要变成矩阵形式
-        W=np.mat(np.ones((n,1)))
-        #X转为矩阵形式
-        xMat = np.mat(X)
-        #y转为矩阵形式，这步非常重要,且要是m x 1的维度格式
-        yMat =np.mat(y.reshape(-1,1))
-        #循环epochs次
-        for i in range(epochs):
-            gradient = xMat.T*(xMat*W-yMat)/m + Lambda * np.sign(W)
-            W=W-a * gradient
-        return W
-    def predict(self,x,w):  #这里的x也要加偏置，训练时x是什么维度的数据，预测也应该保持一样
-        return np.dot(x,w)
+    num = x.shape[0]
+    xlen = x.shape[1]
+    w, b = np.zeros((xlen, 1)), 0
+    for _ in range(epochs):
+        y_hat = np.dot(x, w) + b  
+
+        dw = (np.dot(x.transpose(), (y_hat - y)) / num) + lamda * diff(w)
+        db = np.sum(y_hat - y) / num
+        w -= a * dw
+        b -= a * db
+
+    data = (data - miu) / sigma
+    res = np.dot(w.transpose(), data) + b
+    return res
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
