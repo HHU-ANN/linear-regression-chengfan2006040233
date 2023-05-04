21764fb8687b211f3defa1ce8b9fe1d52fa0f926
chengfan2006040233 <131932921+chengfan2006040233@users.noreply.github.com>
2023-05-04 15:13:19 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -14,21 +14,28 @@ def ridge(data):
     return w@data
     
 def lasso(data):
-    x,y = read_data()
-    #计算总数据量
+     x,y = read_data()
     epochs = 1000
-    Lambda = 0.0001
+    a = 0.00001
+    Lambda = 1
+    #计算总数据量
         m=x.shape[0]
+        #给x添加偏置项
         X = np.concatenate((np.ones((m,1)),x),axis=1)
+        #计算总特征数
         n = X.shape[1]
+        #初始化W的值,要变成矩阵形式
         W=np.mat(np.ones((n,1)))
+        #X转为矩阵形式
         xMat = np.mat(X)
+        #y转为矩阵形式，这步非常重要,且要是m x 1的维度格式
         yMat =np.mat(y.reshape(-1,1))
+        #循环epochs次
         for i in range(epochs):
             gradient = xMat.T*(xMat*W-yMat)/m + Lambda * np.sign(W)
             W=W-a * gradient
         return W
-    def predict(self,x,w):  
+    def predict(self,x,w):  #这里的x也要加偏置，训练时x是什么维度的数据，预测也应该保持一样
         return np.dot(x,w)
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
