cbe04d5abaefa8518efdb1abacd6e8ad6225d394
chengfan2006040233 <131932921+chengfan2006040233@users.noreply.github.com>
2023-05-04 15:03:49 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -14,16 +14,22 @@ def ridge(data):
     return w@data
     
 def lasso(data):
-    X,y = read_data()
-    n = X.shape[1]
-    W=np.mat(np.ones((n,1)))
-    Lambda = 1
+    x,y = read_data()
+    #计算总数据量
     epochs = 1000
-    a = 0.000001
-    for i in range(epochs):
-            gradient = X.T*(X*W-y)/m + Lambda * np.sign(W)
+    Lambda = 0.0001
+        m=x.shape[0]
+        X = np.concatenate((np.ones((m,1)),x),axis=1)
+        n = X.shape[1]
+        W=np.mat(np.ones((n,1)))
+        xMat = np.mat(X)
+        yMat =np.mat(y.reshape(-1,1))
+        for i in range(epochs):
+            gradient = xMat.T*(xMat*W-yMat)/m + Lambda * np.sign(W)
             W=W-a * gradient
-        return W@data
+        return W
+    def predict(self,x,w):  
+        return np.dot(x,w)
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
