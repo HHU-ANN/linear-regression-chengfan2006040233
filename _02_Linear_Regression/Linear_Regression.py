1bdac6e51e27f243c81703ed8243d93a7a467920
chengfan2006040233 <131932921+chengfan2006040233@users.noreply.github.com>
2023-05-04 17:05:48 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -14,47 +14,21 @@ def ridge(data):
    w = np.matmul(np.linalg.inv(np.matmul(X.T,X)+a*np.eye(X.shape[1])),np.matmul(X.T,y))
    return w@data
 
-
-def piandao(w):
-    partial_l1 = [elem for elem in w]
-    for i in range(w.shape[0]):
-        if partial_l1[i] > 0:
-            partial_l1[i] = 1
-        elif partial_l1[i] < 0:
-            partial_l1[i] = -1
-        else:
-            partial_l1[i] = 0
-    partial_l1 = np.array([partial_l1]).transpose()
-    return partial_l1
-
-
 def lasso(data):
-    x, y = read_data()
-    miu = np.mean(x)
-    sigma = np.std(x)
-    for i in range(x.shape[0]):
-        x[i] = (x[i] - miu) / sigma
-
-    y = np.array([y]).transpose()
-
-    lamda = 1
-    step = 0.01
+   def lasso(data):
+    X, y = read_data()
+    w = np.zeros((X.shape[1], 1))
+    limit = 2e-5
+    a = 0.01
+    step = 0.00000001
     epochs = 1000
-    num = x.shape[0]
-    xlen = x.shape[1]
-    w, b = np.zeros((xlen, 1)), 0
-    for _ in range(epochs):
-        y_hat = np.dot(x, w) + b  # 404*1
-
-        dw = (np.dot(x.transpose(), (y_hat - y)) / num) + lamda * piandao(w)
-        db = np.sum(y_hat - y) / num
-
-        w -= step * dw
-        b -= step * db
-
-    data = (data - miu) / sigma
-    res = np.dot(w.transpose(), data) + b
-    return res
+    for i in range(epochs):
+        loss = np.matmul((X - y).transpose(), X - y) + a * np.sum(abs(w))
+        if loss < limit:
+            break
+        dw = np.dot(X - y, X) + np.sign(w)
+        w = w - step * dw
+    return w@data
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
